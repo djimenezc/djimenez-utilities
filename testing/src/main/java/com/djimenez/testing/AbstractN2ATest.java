43cfd1c8b9a52fc29395faad5fe5fa5fc0abe9b8
david.jimenez19 <david.jimenez19@2a284d2a-ed81-11de-bf31-4dec8810c1c1>
2009-12-20 17:21:07 +0000

    create a project skeleton

---
+++ testing/src/main/java/com/djimenez/testing/AbstractN2ATest.java
@@ -0,0 +1,22 @@
+package com.djimenez.testing;
+
+import org.apache.log4j.Logger;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath*:dataBase-tx-config.xml" })
+@Transactional
+/**
+ * Basic test Context Environment
+ * 
+ * @author a.vergara.andres
+ */
+public abstract class AbstractN2ATest {
+
+  protected static final Logger logger =
+    Logger.getLogger(AbstractN2ATest.class);
+
+}
