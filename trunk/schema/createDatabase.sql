4060344b3a206a982f1bd2ca1c84234d08f46659
david.jimenez19 <david.jimenez19@2a284d2a-ed81-11de-bf31-4dec8810c1c1>
2010-03-18 16:43:43 +0000

    Update issue 7 Add ant task and shell script files to compile, load database

---
+++ trunk/schema/createDatabase.sql
@@ -0,0 +1,5 @@
+
+drop database IF EXISTS tmp;
+
+create database tmp;
+use tmp;
---
+++ trunk/scripts/mvninstall-quick.sh
@@ -0,0 +1,64 @@
+#/bin/bash
+
+# export PATH=$PATH:/home/djc001es/programs/maven-2.2.1/bin
+export srcFolder=$1
+export projects=$2
+export errorLogFile=/tmp/n2a-compile-error.log
+export logFile=/tmp/n2a-compile.log
+
+trasformCommaList()
+{
+	echo $1 | sed -e 's/,/ /g'
+}
+
+function log()
+{
+    # Hechamos al /var/log/messages nuestros mensajes    
+    logger "$(basename ${0})" -p local0.info "${1}"
+    echo "$(date) - $(basename ${0}) - ${1} "
+}
+
+# ------------------------------------------------------------------------
+function logInfo()
+{
+    log "[INFO]:  ${1}"
+}
+
+# ------------------------------------------------------------------------
+function logError()
+{
+    log "[ERROR]: ${1}"
+
+}
+
+main()
+{
+	projects=$(trasformCommaList "$projects")
+	
+	if [ -d $projectDir/src ]; then
+		rm $logFile
+		rm $erroLogFile
+	fi
+	touch $errorLogFile
+	touch $logFile
+	
+	for project in $projects
+	do
+		echo Compilando proyecto: $project 
+
+		cd $srcFolder/$project
+ 		mvn install -Dmaven.test.skip=true >> $logFile
+	        res=$?
+		
+		if [ ! $res -eq 0 ];then
+			cd - 1>> $logFile
+			logError "Compilando el proyecto $project"            
+			echo Mirar la traza del error en $logFile
+			exit -1
+       		 fi
+# 		echo Proyecto $project compilado con éxito
+       		 cd - 1> /dev/null
+	done
+}
+
+main;
