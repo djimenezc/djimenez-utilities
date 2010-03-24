d45cc92b5fc383bcae48f41977a4816dcaeb65ee
david.jimenez19 <david.jimenez19@2a284d2a-ed81-11de-bf31-4dec8810c1c1>
2010-03-24 14:39:48 +0000

    Update issue 7 improve the script that summarized the test result

--- scripts/mvnsummary.sh
+++ scripts/mvnsummary.sh
@@ -2,10 +2,50 @@
 
 export logFile="$3/compile.log"
 
-echo Test result summary of $logFile 
+PROJECT_PATH=$1
 
-echo
+process_test_files()
+{
 
-grep "Tests run:" $logFile | grep "Time elapsed:" -v
+	PATH_PROJECT_TESTED=`find -name surefire-reports`
+	
+	for projectFolder in $PATH_PROJECT_TESTED
+	do
 
-exit $salida
+		#projectName=`gawk $PROJECT_FOLDER ' { print "" $1  }' `
+		
+		echo PROJECT-\> $projectFolder
+		
+		testFiles=$(echo `ls $projectFolder | grep .txt`)
+		
+		for testFile in $testFiles
+		do
+			
+			cat $projectFolder/$testFile
+			echo
+		done
+		
+		echo -------------------------------------------------------------------------------
+		echo
+	done
+
+}
+#grep "Tests run:" $logFile | grep "Time elapsed:" -v
+
+printHead()
+{
+	echo Test result summary 
+	
+	echo
+}
+
+main()
+{
+	
+	cd $PROJECT_PATH
+	printHead
+	process_test_files
+	
+}
+
+main
