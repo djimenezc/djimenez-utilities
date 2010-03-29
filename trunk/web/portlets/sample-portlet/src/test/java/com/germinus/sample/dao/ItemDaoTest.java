21f7d2baa251cb81387535d215fb2441a41a01cd
david.jimenez19 <david.jimenez19@2a284d2a-ed81-11de-bf31-4dec8810c1c1>
2010-03-29 16:58:57 +0000

    Update issue 15 Fixed problem with a test

--- trunk/web/portlets/sample-portlet/src/test/java/com/germinus/sample/dao/ItemDaoTest.java
+++ trunk/web/portlets/sample-portlet/src/test/java/com/germinus/sample/dao/ItemDaoTest.java
@@ -1,16 +1,39 @@
 package com.germinus.sample.dao;
 
 import java.util.List;
+import java.util.ResourceBundle;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
 
 import com.germinus.sample.model.Item;
 
-public class ItemDaoTest extends BaseDaoTestCase {
+@SuppressWarnings("deprecation")
+public class ItemDaoTest extends AbstractDependencyInjectionSpringContextTests {
+
+  protected final Log log = LogFactory.getLog(getClass());
+  protected ResourceBundle rb;
 
   @Autowired
   private GenericDao<Item, Long> itemDao;
+
+  @Override
+  protected String[] getConfigLocations() {
+    setAutowireMode(AUTOWIRE_BY_NAME);
+    return new String[] { "classpath:context/applicationContext-resources.xml",
+      "classpath:context/applicationContext-dao.xml", };
+  }
+
+  /**
+   * @param itemDao
+   *          the itemDao to set
+   */
+  public void setItemDao(final GenericDao<Item, Long> itemDao) {
+    this.itemDao = itemDao;
+  }
 
   @Test
   public void testGetItemInvalid() throws Exception {
@@ -29,12 +52,14 @@ public class ItemDaoTest extends BaseDaoTestCase {
 
   @Test
   public void testSaveItem() throws Exception {
+
     final Item newItem = new Item();
+
     newItem.setName("Objetaco");
     newItem.setDescription("Un objeto de prueba");
 
     itemDao.save(newItem);
-    flush();
+    // flush();
 
     log.info("saved new item: " + newItem.toString());
 
@@ -42,4 +67,5 @@ public class ItemDaoTest extends BaseDaoTestCase {
 
     org.junit.Assert.assertNotNull(items.size() > 0);
   }
+
 }
