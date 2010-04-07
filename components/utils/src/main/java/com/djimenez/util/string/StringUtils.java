db60308ad14b6d67c8a6cbf926b3a2df6ff5a63a
david.jimenez19 <david.jimenez19@2a284d2a-ed81-11de-bf31-4dec8810c1c1>
2010-04-07 13:14:05 +0000

    Update issue 14 Add class to clean a string of strange simbols

---
+++ components/utils/src/main/java/com/djimenez/util/string/StringUtils.java
@@ -0,0 +1,59 @@
+package com.djimenez.util.string;
+
+public class StringUtils {
+
+  public static String deleteStrangeSimbols(final String string) {
+
+    String encodeString = new String(string);
+
+    encodeString = encodeString.replace('Á', 'a');
+    encodeString = encodeString.replace('É', 'e');
+    encodeString = encodeString.replace('Í', 'i');
+    encodeString = encodeString.replace('Ó', 'o');
+    encodeString = encodeString.replace('Ú', 'u');
+    encodeString = encodeString.replace('á', 'a');
+    encodeString = encodeString.replace('é', 'e');
+    encodeString = encodeString.replace('í', 'i');
+    encodeString = encodeString.replace('ó', 'o');
+    encodeString = encodeString.replace('ú', 'u');
+
+    encodeString = encodeString.replace('ñ', 'n');
+    encodeString = encodeString.replace('ç', 'c');
+
+    encodeString = encodeString.replace('ä', 'a');
+    encodeString = encodeString.replace('ë', 'e');
+    encodeString = encodeString.replace('ï', 'i');
+    encodeString = encodeString.replace('ö', 'o');
+    encodeString = encodeString.replace('ü', 'u');
+    encodeString = encodeString.replace('Ä', 'a');
+    encodeString = encodeString.replace('Ë', 'e');
+    encodeString = encodeString.replace('Ï', 'i');
+    encodeString = encodeString.replace('Ö', 'o');
+    encodeString = encodeString.replace('Ü', 'u');
+
+    encodeString = encodeString.replace('à', 'a');
+    encodeString = encodeString.replace('è', 'e');
+    encodeString = encodeString.replace('ì', 'i');
+    encodeString = encodeString.replace('ò', 'o');
+    encodeString = encodeString.replace('ù', 'u');
+    encodeString = encodeString.replace('À', 'a');
+    encodeString = encodeString.replace('È', 'e');
+    encodeString = encodeString.replace('Ì', 'i');
+    encodeString = encodeString.replace('Ò', 'o');
+    encodeString = encodeString.replace('Ù', 'u');
+
+    encodeString = encodeString.replace('Â', 'a');
+    encodeString = encodeString.replace('Ê', 'e');
+    encodeString = encodeString.replace('Î', 'i');
+    encodeString = encodeString.replace('Ô', 'o');
+    encodeString = encodeString.replace('Û', 'u');
+    encodeString = encodeString.replace('â', 'a');
+    encodeString = encodeString.replace('ê', 'e');
+    encodeString = encodeString.replace('î', 'i');
+    encodeString = encodeString.replace('ô', 'o');
+    encodeString = encodeString.replace('û', 'u');
+
+    return encodeString;
+  }
+
+}
