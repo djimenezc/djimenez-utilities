084965b39f97c6f659650fe80a4cc16789765453
david.jimenez19 <david.jimenez19@2a284d2a-ed81-11de-bf31-4dec8810c1c1>
2010-04-28 11:47:51 +0000

    Update issue 7 Fixed problem with the ant task, now the ant build file get the properties from maven

---
+++ trunk/web/spring-mvc/spring-mvc-model/src/main/java/com/djimenez/example/spring/mvc/constants/Constants.java
@@ -0,0 +1,24 @@
+package com.djimenez.example.spring.mvc.constants;
+
+/**
+ * Constant values used throughout the application.
+ * 
+ * @author <a href="mailto:matt@raibledesigns.com">Matt Raible</a>
+ */
+public final class Constants {
+
+  /**
+   * The name of the ResourceBundle used in this application
+   */
+  public static final String BUNDLE_KEY = "ApplicationResources";
+
+  /**
+   * File separator from System properties
+   */
+  public static final String FILE_SEP = System.getProperty("file.separator");
+
+  private Constants() {
+    // hide me
+  }
+
+}
