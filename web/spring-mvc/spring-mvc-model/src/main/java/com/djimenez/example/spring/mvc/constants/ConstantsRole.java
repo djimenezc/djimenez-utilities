cc414795cca8f404e2b74c6d762e0bf264e18d1a
david.jimenez19 <david.jimenez19@2a284d2a-ed81-11de-bf31-4dec8810c1c1>
2010-04-30 09:28:55 +0000

    Update issue  18 Add new test to increase the coverage of the project, this test have been gotten of appfuse project

---
+++ web/spring-mvc/spring-mvc-model/src/main/java/com/djimenez/example/spring/mvc/constants/ConstantsRole.java
@@ -0,0 +1,23 @@
+package com.djimenez.example.spring.mvc.constants;
+
+public final class ConstantsRole {
+
+  /**
+   * The name of the Administrator role, as specified in web.xml
+   */
+  public static final String ADMIN_ROLE = "ROLE_ADMIN";
+
+  /**
+   * The name of the User role, as specified in web.xml
+   */
+  public static final String USER_ROLE = "ROLE_USER";
+
+  /**
+   * The name of the user's role list, a request-scoped attribute when
+   * adding/editing a user.
+   */
+  public static final String USER_ROLES = "userRoles";
+
+  private ConstantsRole() {
+  }
+}
