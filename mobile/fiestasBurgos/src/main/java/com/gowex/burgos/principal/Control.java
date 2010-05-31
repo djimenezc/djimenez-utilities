e9aa8ef148126184a0cdbc54f68dfbda04059e31
david.jimenez19 <david.jimenez19@2a284d2a-ed81-11de-bf31-4dec8810c1c1>
2010-05-31 14:01:42 +0000

    Update issue 18 comment entry point, the application start directly by the menu, it is not working the transation between screens.

--- mobile/fiestasBurgos/src/main/java/com/gowex/burgos/principal/Control.java
+++ mobile/fiestasBurgos/src/main/java/com/gowex/burgos/principal/Control.java
@@ -1,90 +1,91 @@
 // Decompiled by Jad v1.5.8e. Copyright 2001 Pavel Kouznetsov.
 // Jad home page: http://www.geocities.com/kpdus/jad.html
-// Decompiler options: packimports(3) 
+// Decompiler options: packimports(3)
 
 package com.gowex.burgos.principal;
 
-import com.gowex.burgos.forms.f;
-import com.gowex.burgos.forms.g;
 import javax.microedition.lcdui.Display;
 import javax.microedition.midlet.MIDlet;
 
-public class Control extends MIDlet
-{
+import com.gowex.burgos.forms.f;
+import com.gowex.burgos.forms.g;
 
-    public Control()
-    {
-        a_int_fld = -1;
-    }
+public class Control extends MIDlet {
 
-    public void startApp()
-    {
-        a_javax_microedition_lcdui_Display_fld = Display.getDisplay(this);
-        actualizaEstado();
-    }
+  private Display a_javax_microedition_lcdui_Display_fld;
 
-    public void actualizaEstado()
-    {
-        a_int_fld++;
-        switch(a_int_fld)
-        {
-        case 0: // '\0'
-            a();
-            return;
+  public g miInterfaz;
 
-        case 1: // '\001'
-            b();
-            return;
+  public static int LeftSoftKey = 0;
 
-        case 2: // '\002'
-            c();
-            return;
+  public static int RightSoftKey = 0;
 
-        case 3: // '\003'
-            b();
-            return;
+  public static int pantallaAncho = 0;
 
-        case 4: // '\004'
-            d();
-            break;
-        }
-    }
+  public static int pantallaAlto = 0;
 
-    private void a()
-    {
-        (new f(this, a_javax_microedition_lcdui_Display_fld, "portada0.png", 2000, true, false, 0xffffff)).setFullScreenMode(true);
-    }
+  private final int a_int_fld;
 
-    private void b()
-    {
-        (new f(this, a_javax_microedition_lcdui_Display_fld, "portada1.png", 2500, false, false, 0xffffff)).setFullScreenMode(true);
-    }
+  public Control() {
+    a_int_fld = -1;
+  }
 
-    private void c()
-    {
-        miInterfaz = new g(this, a_javax_microedition_lcdui_Display_fld);
-    }
+  private void a() {
+    (new f(this, a_javax_microedition_lcdui_Display_fld, "portada0.png", 2000,
+      true, false, 0xffffff)).setFullScreenMode(true);
+  }
 
-    private void d()
-    {
-        destroyApp(true);
-        notifyDestroyed();
-    }
+  public void actualizaEstado() {
 
-    public void pauseApp()
-    {
-    }
+    c();
+    // a_int_fld++;
+    // switch(a_int_fld)
+    // {
+    // case 0: // '\0'
+    // a();
+    // return;
+    //
+    // case 1: // '\001'
+    // b();
+    // return;
+    //
+    // case 2: // '\002'
+    // c();
+    // return;
+    //
+    // case 3: // '\003'
+    // b();
+    // return;
+    //
+    // case 4: // '\004'
+    // d();
+    // break;
+    // }
+  }
 
-    public void destroyApp(boolean flag)
-    {
-    }
+  private void b() {
+    (new f(this, a_javax_microedition_lcdui_Display_fld, "portada1.png", 2500,
+      false, false, 0xffffff)).setFullScreenMode(true);
+  }
 
-    private Display a_javax_microedition_lcdui_Display_fld;
-    public g miInterfaz;
-    public static int LeftSoftKey = 0;
-    public static int RightSoftKey = 0;
-    public static int pantallaAncho = 0;
-    public static int pantallaAlto = 0;
-    private int a_int_fld;
+  private void c() {
+    miInterfaz = new g(this, a_javax_microedition_lcdui_Display_fld);
+  }
+
+  private void d() {
+    destroyApp(true);
+    notifyDestroyed();
+  }
+
+  public void destroyApp(final boolean flag) {
+  }
+
+  public void pauseApp() {
+  }
+
+  public void startApp() {
+    a_javax_microedition_lcdui_Display_fld = Display.getDisplay(this);
+    actualizaEstado();
+  }
 
 }
