74a308dde615a2f958d76ac85ce51e45e8b2186c
david.jimenez19 <david.jimenez19@2a284d2a-ed81-11de-bf31-4dec8810c1c1>
2010-06-02 08:23:25 +0000

    Fixes issue 28 Delete project because there was a error in the test after the generating with spring-roo. Reset project

--- trunk/web/spring-roo/roo-gwt/src/main/java/com/djimenez/roo/gwt/gwt/request/ApplicationRequestFactory.java
+++
@@ -1,5 +0,0 @@
-package com.djimenez.roo.gwt.gwt.request;
-import com.google.gwt.requestfactory.shared.RequestFactory;
-public interface ApplicationRequestFactory extends RequestFactory {
-    PersonRequest personRequest();
-}
--- trunk/web/spring-roo/roo-gwt/src/test/java/com/djimenez/spring/roo/model/PersonDataOnDemand.java
+++
@@ -1,8 +0,0 @@
-package com.djimenez.spring.roo.model;
-
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-import com.djimenez.spring.roo.model.Person;
-
-@RooDataOnDemand(entity = Person.class)
-public class PersonDataOnDemand {
-}
