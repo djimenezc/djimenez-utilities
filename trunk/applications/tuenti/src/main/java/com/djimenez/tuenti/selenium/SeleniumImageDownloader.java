b74353bb9427b00c5c02f6bd4ce16f9b3d8263e6
david.jimenez19 <david.jimenez19@2a284d2a-ed81-11de-bf31-4dec8810c1c1>
2011-10-22 10:00:14 +0000

    added last changes, appear the save windows but it is not possible to click in the ok button

--- trunk/applications/tuenti/src/main/java/com/djimenez/tuenti/selenium/SeleniumImageDownloader.java
+++ trunk/applications/tuenti/src/main/java/com/djimenez/tuenti/selenium/SeleniumImageDownloader.java
@@ -3,9 +3,27 @@ package com.djimenez.tuenti.selenium;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
 
+import org.openqa.selenium.server.SeleniumServer;
+
 import com.thoughtworks.selenium.DefaultSelenium;
 
 public class SeleniumImageDownloader {
+
+  public static void main(final String[] args) throws Exception {
+
+    SeleniumServer seleniumServer;
+
+    try {
+      seleniumServer = new SeleniumServer();
+      seleniumServer.start();
+    }
+    catch (final Exception e) {
+    }
+
+    final SeleniumImageDownloader downloader = new SeleniumImageDownloader();
+
+    downloader.getImage();
+  }
 
   public void getImage() throws Exception {
     String[] commands = new String[] {};
@@ -25,6 +43,8 @@ public class SeleniumImageDownloader {
     robot.keyPress(KeyEvent.VK_S);
     robot.keyRelease(KeyEvent.VK_S);
     robot.keyRelease(KeyEvent.VK_CONTROL);
+    robot.keyPress(KeyEvent.VK_ACCEPT);
+    robot.keyRelease(KeyEvent.VK_ACCEPT);
     // once the dialog box is displayed, the autoIt script
     // will fire off and send an Enter command.
   }
